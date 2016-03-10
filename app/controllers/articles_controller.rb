69a7f9d91ff82fbadf2ee9dd6f5b56d95c31fa31
Michelle Colon <michellecolon.mc@gmail.com>
2016-03-09 23:37:56 -0500

    HW 7
    Broke in the controller and I don't know how

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+  def index #find all articles from database
+    #Article.all runs: SELECT * FROM articles
+    @articles = Article.all
+  end
+  def new
+    @article = Article.new
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def create
+    puts 'Creating article'
+    @article = Article.create(article_params)
+
+  redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
